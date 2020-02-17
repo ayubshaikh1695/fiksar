4f7f9cf4a446353aadb9aface0b729704fa4c03a
Ayub Hossain <ayubshaikh1695@gmail.com>
2020-02-18 01:28:09 +0530

    added FiksarLogoComponent to declarations

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FiksarLogoComponent } from './fiksar-logo/fiksar-logo.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FiksarLogoComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [FiksarLogoComponent]
 })
 export class ComponentsModule { }
