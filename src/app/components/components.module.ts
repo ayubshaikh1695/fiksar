be24b82b01da6892f3f3ce0cfb55f19f93fded41
Ayub Hossain <ayubshaikh1695@gmail.com>
2020-02-18 20:23:23 +0530

    added NavsComponent to declarations and exports list

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FiksarLogoComponent } from './fiksar-logo/fiksar-logo.component';
+import { NavsComponent } from './navs/navs.component';
 
 
 
 @NgModule({
-  declarations: [FiksarLogoComponent],
+  declarations: [FiksarLogoComponent, NavsComponent],
   imports: [
     CommonModule
   ],
-  exports: [FiksarLogoComponent]
+  exports: [FiksarLogoComponent, NavsComponent]
 })
 export class ComponentsModule { }
