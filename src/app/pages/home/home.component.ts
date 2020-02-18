cf1ace832de3c73dbf22c5e019d26f2efc3c74f8
Ayub Hossain <ayubshaikh1695@gmail.com>
2020-02-19 01:13:53 +0530

    added property to store relative path of hero image

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  heroImagePath = '../../../assets/images/kari-shea-207221.png';
 
   constructor() { }
 
