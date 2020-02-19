5866eb261d639ec97d5802d06bd630f91f88bb27
Ayub Hossain <ayubshaikh1695@gmail.com>
2020-02-19 22:35:34 +0530

    created component for search

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FiksarLogoComponent } from './fiksar-logo/fiksar-logo.component';
 import { NavsComponent } from './navs/navs.component';
+import { SearchComponent } from './search/search.component';
 
 
 
 @NgModule({
-  declarations: [FiksarLogoComponent, NavsComponent],
+  declarations: [FiksarLogoComponent, NavsComponent, SearchComponent],
   imports: [
     CommonModule
   ],
