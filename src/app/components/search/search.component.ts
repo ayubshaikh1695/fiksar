becfb7342e4d52fda84d416b0a01988a7562ae6b
Ayub Hossain <ayubshaikh1695@gmail.com>
2020-02-20 01:24:23 +0530

    added search component DOM elements and added their custom styles

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  @Input() placeholder: string;
+  @Input() buttonLabel: string;
 
   constructor() { }
 
