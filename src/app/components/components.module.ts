5cc9985beb4605ea0068785887ad00cf19e9e254
Ayub Hossain <ayubshaikh1695@gmail.com>
2020-02-24 00:01:58 +0530

    added NavbarComponent and HeaderComponent to declarations and exports

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { FiksarLogoComponent } from './fiksar-logo/fiksar-logo.component';
 import { NavsComponent } from './navs/navs.component';
 import { SearchComponent } from './search/search.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [FiksarLogoComponent, NavsComponent, SearchComponent],
+  declarations: [FiksarLogoComponent, NavsComponent, SearchComponent, NavbarComponent, HeaderComponent],
   imports: [
     CommonModule
   ],
-  exports: [FiksarLogoComponent, NavsComponent, SearchComponent]
+  exports: [FiksarLogoComponent, NavsComponent, SearchComponent, NavbarComponent, HeaderComponent]
 })
 export class ComponentsModule { }
